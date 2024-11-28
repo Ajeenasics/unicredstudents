5de6b7c9348fe576f00cc4c81eeef8b0b7a54b31
indrajask <indraja.suchithra.k@gmail.com>
2024-11-28 11:44:57 +0530

    clerk dashboard complete

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import ClerkDashboard from './Components/Clerk/ClerkDashboard';
 
 //import Custcreditapply from './Components/Custcreditapply';
 //import Custcreditapplystatus from './Components/Custcreditapplystatus';
-import Custloanapply from './Components/Custloanapply';
+// import Custloanapply from './Components/Custloanapply';
 // import Custloanapplystatus from './Components/Custloanapplystatus';
-import CustLoanDetails from './Component/CustLoanDetails';
-import CustomerLoanIdentity from './Component/CustomerLoanIdentity';
-import CustLoanEmplDetails from './Component/CustLoanEmplDetails';
+// import CustLoanDetails from './Component/CustLoanDetails';
+// import CustomerLoanIdentity from './Component/CustomerLoanIdentity';
+// import CustLoanEmplDetails from './Component/CustLoanEmplDetails';
 
 
 function App() {
@@ -16,12 +16,12 @@ function App() {
     <div>
 
 
-      <Custloanapply/>
+      {/* <Custloanapply/> */}
       {/* <Custloanapplystatus/> */}
       {/* <Custcreditapply/> */}
       {/* <Custcreditapplystatus/> */}
 
-      <CustLoanEmplDetails/>
+      {/* <CustLoanEmplDetails/> */}
 
 
       <ClerkDashboard/>
