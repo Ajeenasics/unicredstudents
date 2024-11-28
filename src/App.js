c4e8535a40538eaef85d054d5fbf2623370d8b16
Nandanasureshss <nandananavya156@gmail.co>
2024-11-28 10:55:58 +0530

    Customer personal Identity Employment details

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import CustLoanDetails from './Component/CustLoanDetails';
-import CustomerLoanIdentity from './Component/CustomerLoanIdentity';
-import CustLoanEmplDetails from './Component/CustLoanEmplDetails';
+// import CustomerLoanIdentity from './Component/CustomerLoanIdentity';
+// import CustLoanEmplDetails from './Component/CustLoanEmplDetails';
 
 function App() {
   return (
     <div>
-      <CustLoanEmplDetails/>
+      <CustLoanDetails
+      />
     </div>
   );
 }
