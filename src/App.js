1548f5ddf5fdea3c4d8b1d98a6b3bfdfd6c07d57
sumitha2003 <sumithadev5226@gmail.com>
2024-11-30 19:38:05 +0530

    Custcreditcarddetails and Custloanappdetail

--- src/App.js
+++ src/App.js
@@ -8,20 +8,21 @@ import Custloanapply from './Components/Custloanapply';
 import CustLoanDetails from './Component/CustLoanDetails';
 import CustomerLoanIdentity from './Component/CustomerLoanIdentity';
 import CustLoanEmplDetails from './Component/CustLoanEmplDetails';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Custloanappdetail from './Componentcust/Custloanappdetail';
+import Custcreditcarddetails from './Componentcust/Custcreditcarddetails';
 
 
 function App() {
   return (
-    <div>
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path='/Custloanappdetail' element={<Custloanappdetail/>}/>
+      <Route path='/creditcard' element={<Custcreditcarddetails/>}/>
 
-      <Custloanapply/>
-      {/* <Custloanapplystatus/> */}
-      {/* <Custcreditapply/> */}
-      {/* <Custcreditapplystatus/> */}
-
-      <CustLoanEmplDetails/>
-
-    </div>
+    </Routes>
+     </BrowserRouter>
   );
 }
 
